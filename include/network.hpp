99989ed9e2b328386b268938ee4a5c9b41f1b92b
Leo Branchut <leo.branchut@gmail.com>
2022-03-22 23:27:35 +0100

    Support mesh init, closes points scan and save + Attributing points informations to its internal config (mesh_id, type, status)

--- include/network.hpp
+++ include/network.hpp
@@ -4,8 +4,27 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include "internal.hpp"
+
+
+#define POINT_NB_PER_MESH 20
+
+typedef struct network_point {
+    uint32_t id;
+    uint8_t status;
+    uint8_t type;
+    uint16_t mesh_id;               /*Light id inside the mesh. ID unattributed: 33*/
+}t_network_points;
+
+t_network_points T_MESH_POINT[POINT_NB_PER_MESH];
+
 /*
- * Start mesh recognition. Scan closes lights and join them to the mesh.
+ * Add a light to the network known points
+ */
+int network_add_a_point(uint32_t id, uint8_t status, uint8_t type);
+
+/*
+ * Start mesh recognition. Send a message to clothes lights.
  * Returns 0 if OK
  */
 int start_mesh_initialisation(void);
@@ -23,7 +42,7 @@ int pick_closest_lights(void);
 
 /*
  * Add a light to a mesh.
-* Returns 0 if OK
+ * Returns 0 if OK
  */
 int add_light_to_the_mesh(int *mesh, int light_id);
 
--- src/network.cpp
+++ src/network.cpp
@@ -3,15 +3,30 @@
  */
 #include "network.hpp"
 #include "lora_handling.hpp"
+#include <FreeRTOS.h>
+#include "task.h"
 #include <RH_RF95.h>
+#include "semphr.h"
+
+static SemaphoreHandle_t mutex;
+//#define TO_ASK_ATTENDANCE 2000/portTICK_PERIOD_MS
 
 int start_mesh_initialisation(void) {
 
     uint8_t power;
+    uint8_t ret;
 
-    scan_lights(power);
+    ret = scan_lights(power);
 
-    return 0;
+    if(ret != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int network_initialize_close_points(void) {
+
 }
 
 int scan_lights(uint8_t tx_power) {
@@ -19,16 +34,44 @@ int scan_lights(uint8_t tx_power) {
     /*Reconfigure the transmition power*/
     lora_change_power(tx_power);
 
-    /*Send a request message*/
+    /*Send a request message to get closest lights id*/
     message_t request;
     request.source_id = internal_config_get_self_id();
     
     if(request.source_id){
-        return -1;
+        return EXIT_FAILURE;
     }
     
     request.destination_id = UNIVERSAL_CALL;
-    request.message_action = ACTION_RQST_ACK;
+    request.message_action = ACTION_RQST_ATTENDANCE_ACK;
 
     lora_send((uint8_t*) &request, sizeof(request));
+
+    return EXIT_SUCCESS;
 }
+
+int network_add_a_point(uint32_t id, uint8_t status, uint8_t type) {
+    uint8_t index;
+    uint8_t placed = -1;
+
+    for(index = 0; ((index < POINT_NB_PER_MESH) && (placed < 0)); index++ )
+    {
+        if(!T_MESH_POINT[index].id) {
+            T_MESH_POINT[index].id = id;
+            T_MESH_POINT[index].status = status;
+            T_MESH_POINT[index].type = type;
+            T_MESH_POINT[index].mesh_id = index;
+            placed = index;
+        }
+    }
+
+    /*Returns 1 if error*/
+    return placed;
+}
+
+/*TODO: Fonction d'init network pour organiser tout ça.
+    -> start mesh init
+    -> Réception des ack, attribution des id et renvoi aux points
+    -> Une fois le tableau plein,la maille est constituée
+    -> A définir: lancement du network init à la réception d'un message ou au démarrage ??
+*/
